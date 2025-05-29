603fe46f71b092873b43f8a0be1b50ce0b64e432
Rijkkie <78078877+Rijkkie@users.noreply.github.com>
2025-05-29 02:10:12 +0200

    Update process.py
    Use xgb

--- process.py
+++ process.py
@@ -93,7 +93,7 @@ class Uls23(SegmentationAlgorithm):
 
         self.load_model()
         spacings = self.load_data()
-        predictions = self.predict(spacings)
+        predictions = self.predict_with_classifier(spacings)
         self.postprocess(predictions)
 
         end_time = time.time()
